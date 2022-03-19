2947f275f78f8aac220b3f07579b49024dfffd2d
specbug <rishit7.rv@gmail.com>
2022-03-19 16:22:47 +0530

    jsonify cmap

--- cmap.py
+++ cmap.py
@@ -1,9 +1,7 @@
-from dataclasses import dataclass
 from typing import List, Optional
 from collections import defaultdict
 
 
-@dataclass
 class CNode:
     def __init__(self, url: str, title: str, desc: str, name: Optional[str] = None):
         self.url = url
@@ -11,12 +9,13 @@ class CNode:
         self.name = name
         self.title = title
         self.children = []
+        self.__post_init__()
 
     def __post_init__(self):
         self.title = self.title.strip().capitalize()
         self.desc = self.desc.strip()
         if self.name is None:
-            self.name = self.title[:7]
+            self.name = self.title[:15]
         self.name = self.name.strip().lower()
 
     def get_children(self) -> List['CNode']:
@@ -25,10 +24,28 @@ class CNode:
     def add_children(self, children: List['CNode']):
         self.children.extend(children)
 
+    def json(self) -> dict:
+        return dict(
+            name=self.name,
+            title=self.title,
+            desc=self.desc,
+            url=self.url,
+            children=[child.json() for child in self.children]
+        )
+
 
 class CMap:
     def __init__(self):
         self._states = defaultdict(None)
+        self._root = None
+
+    @property
+    def root(self):
+        return self._root
+
+    @root.setter
+    def root(self, node: CNode):
+        self._root = node
 
     @property
     def states(self) -> List[CNode]:
@@ -38,9 +55,10 @@ class CMap:
     def size(self):
         return len(self.states)
 
-    def add(self, url: str, **kwargs):
+    def add(self, url: str, **kwargs) -> CNode:
         node = CNode(url=url, **kwargs)
         self._states[url] = node
+        return node
 
     def get(self, url: str) -> Optional[CNode]:
         return self._states.get(url)
@@ -52,8 +70,12 @@ class CMap:
     def destroy(self):
         self._states.clear()
 
-    def show(self, node: CNode, indent: int = 4):
+    def show(self, node: CNode, indent: int = 2):
         representation = [f'–{node.name.split("-")[-1]}']
         for child in node.children:
             representation.extend(['\n', ' ' * indent, self.show(child, indent * 2)])
         return ''.join(representation)
+
+    def json(self):
+        return self.root.json()
+
