b8915fe58d22e33f5de1708a7160afa619908b26
specbug <rishit7.rv@gmail.com>
2022-03-20 04:11:39 +0530

    plot 2.0

--- cmap.py
+++ cmap.py
@@ -25,6 +25,10 @@ class CMap:
     def root(self):
         return self._root
 
+    @root.setter
+    def root(self, root: str):
+        self._root = root
+
     @property
     def size(self):
         return len(self._edges)
@@ -60,12 +64,29 @@ class CMap:
         self._graph = nx.read_gpickle(filename)
         return self._graph
 
-    def plot(self, filename: Optional[str] = None, height: int = 800, width: int = 800):
+    def plot(self, filename: Optional[str] = None, height: int = 1200, width: str = '100%'):
         net = Network(height=height, width=width, directed=True, heading=self._root)
-        net.from_nx(self._graph, default_node_size=15)
+        net.from_nx(self._graph)
+        # net.show_buttons(filter_=['physics'])
+        net.set_options(
+            """
+            var options = {
+              "physics": {
+                "barnesHut": {
+                  "gravitationalConstant": -15000,
+                  "centralGravity": 1
+                },
+                "minVelocity": 0.75
+              }
+            }
+            """
+        )
         for node in net.nodes:
+            node['size'] = 15
             node['label'] = ''
             node['title'] = f'<a href="{node["id"]}">{node["id"]}</a>'
+            if node['id'] == self._root:
+                node['color'] = '#ff0000'
         net.save_graph(filename or f'{str(tldextract.extract(self.root).domain)}.html')
 
 
