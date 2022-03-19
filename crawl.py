49a5a4dc75252d8f4fd312af1d8f83c3dfb4ab34
specbug <rishit7.rv@gmail.com>
2022-03-19 23:51:50 +0530

    limit concurrent tasks

--- crawl.py
+++ crawl.py
@@ -8,13 +8,16 @@ from typing import List, Optional
 from cmap import CMap
 from resource import Resource
 
+sem = None
+connector = None
+
 
 async def crawl(url: str, parent: Optional[str] = None):
     global c_map
     tasks = []
     try:
         resource = Resource(url=url)
-        await resource.parse()
+        await resource.parse(connector=connector)
     except Exception as exc:
         print(f'Warning! Failed to parse {url}: {exc}')
         return
@@ -26,14 +29,28 @@ async def crawl(url: str, parent: Optional[str] = None):
     c_map.connect(parent, resource.url)
     for child_url in resource.links:
         if not c_map.connected(resource.url, child_url):
-            tasks.append(crawl(child_url, resource.url))
+            async with sem:
+                tasks.append(
+                    asyncio.ensure_future(crawl(child_url, resource.url))
+                )
     await asyncio.gather(*tasks)
 
-uri = 'http://www.paulgraham.com/'
-# TODO: support max breadth and depth
-sweep_kernel = (4, 3)  # (breadth, depth)
-t0 = time.time()
-c_map = CMap()
-asyncio.run(crawl(uri))
-print(f'Crawled {c_map.size} internal linkmaps in {time.time() - t0} s')
-print(c_map.edges)
+
+async def main(url):
+    global sem, connector
+    sem = asyncio.Semaphore(20)
+    connector = aiohttp.TCPConnector(limit=20)
+    await crawl(url)
+    await connector.close()
+
+
+if __name__ == '__main__':
+    uri = 'https://www.waitbutwhy.com/'
+    # TODO: support max breadth and depth
+    sweep_kernel = (4, 3)  # (breadth, depth)
+    t0 = time.time()
+    c_map = CMap()
+    asyncio.run(main(uri))
+    print(f'Crawled {c_map.size} internal linkmaps in {time.time() - t0} s')
+    c_map.cart()
+    c_map.plot()
