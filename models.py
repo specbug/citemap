4cf75256412562e8994f48df173d4225cd9599a8
specbug <rishit7.rv@gmail.com>
2022-03-20 15:37:44 +0530

    checks

--- models.py
+++ models.py
@@ -4,7 +4,14 @@ from pydantic import BaseModel, Field
 from typing import List, Optional, Union
 
 
-class Plot(BaseModel):
-    filename: str = Field(..., description="output file name (.html)", example="plot.html", regex=r'\.html$')
+class Arg(BaseModel):
+    url: str = Field(
+        ...,
+        description="URL of the blog",
+        regex=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/"
+              r")(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s("
+              r")<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    )
+    filename: str = Field(..., description="output file name (.html)", example="plot.html", regex=r'\w+\.html')
     height: Optional[Union[int, str]] = 1200
     width: Optional[Union[int, str]] = '70%'
