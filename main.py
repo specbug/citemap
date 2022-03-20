50ca193519c644cdf0267497a1b242ed5d140bf0
specbug <rishit7.rv@gmail.com>
2022-03-20 21:11:18 +0530

    gc

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import asyncio
 import uvicorn
 from app import app
 
-HOST = os.getenv('SERVER_HOST', '0.0.0.0')
-PORT = int(os.getenv('SERVER_PORT', 9271))
+HOST = os.getenv('HOST', '0.0.0.0')
+PORT = int(os.getenv('PORT', 9271))
 
 if __name__ == '__main__':
+    print(f'Starting server at {HOST}:{PORT}')
     uvicorn.run(app, host=HOST, port=PORT)
 
